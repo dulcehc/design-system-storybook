b9333d00bfb73f5825851a1f38ef0d0fbd6f6bcb
Dulce Hernandez <dulce.hernandezc@gmail.com>
2019-10-29 20:24:38 -0600

    Add stories

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = function ({
+    config
+}) {
+    config.module.rules.unshift({
+        test: /\.stories\.jsx?$/,
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        enforce: 'pre',
+    });
+
+    return config;
+};
