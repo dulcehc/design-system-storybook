ae39039d391e5e4e82fafe601815120a6aa1c376
Dulce Hernandez <dulce.hernandezc@gmail.com>
2019-10-31 08:39:04 -0600

    Add app component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Button } from './Button'
+
+function App() {
+  return (
+    <div>
+      <Button appearance="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App
